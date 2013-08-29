fb76dfe4a411683dbe82628e33dd24d6a4c5b742
botp <botpena@gmail.com>
2013-08-29 11:18:25 +0800

    init w database.yml. no models.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dar::Application.config.secret_key_base = '93658982353c103e7b0b0419fa4308e3a706c59a1a1193e752ab611404e7a6f2017508fea245fc436787a365ec62572549efc65d9919d1d319018a7fcce5c32c'
