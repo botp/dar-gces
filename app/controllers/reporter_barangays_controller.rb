1d9d1e3ec3930fab53164f1ae16cabae419d9e75
botp <botpena@gmail.com>
2013-09-12 01:33:46 +0800

    included reports selection for reporter_barangays via checkbox_tag.  made some late* through-associations bw arc and barangay

--- app/controllers/reporter_barangays_controller.rb
+++ app/controllers/reporter_barangays_controller.rb
@@ -1,13 +1,31 @@
 
 class ReporterBarangaysController < ApplicationController
+  def initialize
+    @reports_program_hash = {
+      "Geographics" => "barangay_geographicals" ,
+      "Population" => "barangay_populations" ,
+    }
+    @reports = @reports_program_hash.keys
+
+    # @reports = [
+    #   "Geographics", 
+    #   "Population", 
+    # ]
+
+
+    @barangays = Barangay.none
+    @reports_selection = []
+  end
+
   def index
     @barangays=Barangay.all  
-   end
+  end
 
 
   def show
     if params[:search]
       @barangays = Barangay.search(params[:search])
+      @reports_selection = params[:reports]
     else
       redirect_to action: 'index'
     end
--- app/models/barangay.rb
+++ app/models/barangay.rb
@@ -1,6 +1,7 @@
 class Barangay < ActiveRecord::Base
   belongs_to :municipality
   has_one :arc_barangay
+  has_one :arc, :through => :arc_barangay
   has_many :barangay_geographicals
   has_many :barangay_politicals
   has_many :barangay_populations
