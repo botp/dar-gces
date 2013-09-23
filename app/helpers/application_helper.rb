6b8149cc86af885fbef0d4ae96890f9927bea4d9
botp <botpena@gmail.com>
2013-09-23 21:03:25 +0800

    created new helpers buttonlink_to as repl of some link_to. update other views accordingly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   # creates a link of subtitle w back link to main data entry listing  
-  def link_to_subtitle(subtitle,backlink=nil)
-    link_to subtitle, backlink || "/data_entry" 
+  def backlink_to text,backlink=nil
+    link_to text, backlink || "/data_entry" 
   end
+
+  # create a link of type button
+  def buttonlink_to text, path, klass={}
+    link_to text, path, klass.merge(class: "btn btn-primary")
+  end  
 end
