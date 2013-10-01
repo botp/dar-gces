6efe09414b7bc0b60af2be39ebad767e8af77968
botp <botpena@gmail.com>
2013-10-01 09:41:50 +0800

    test config.serve_static_assets=true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ Dar::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
+  # modified by botp 20131009
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
